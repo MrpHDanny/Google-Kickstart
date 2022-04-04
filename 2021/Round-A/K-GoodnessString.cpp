838288abbff4a8a963f55e296304fcb8546fb4e3
Daniel G <danielis.golu@gmail.com>
2022-04-04 14:19:10 +0100

    Update and rename problem_1.cpp to K-GoodnessString.cpp

--- 2021/Round-A/K-GoodnessString.cpp
+++ 2021/Round-A/K-GoodnessString.cpp
@@ -5,7 +5,9 @@
 using namespace std;
 
 string solution(int N, int K);
-
+/* Problem Link
+https://codingcompetitions.withgoogle.com/kickstart/round/0000000000436140/000000000068cca3
+*/
 int main()
 {
     int T; cin >> T;    // Test cases
